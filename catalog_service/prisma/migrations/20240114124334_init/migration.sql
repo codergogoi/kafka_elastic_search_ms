4b791cddf9fc29292e35b6065f1be6bb5511c1b9
jay <codergogoi@gmail.com>
2024-01-18 19:08:51 +0100

    episode 9

---
+++ catalog_service/prisma/migrations/20240114124334_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL,
+    "stock" INTEGER NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
--- catalog_service/src/repository/catalog.repository.ts
+++ catalog_service/src/repository/catalog.repository.ts
@@ -1,26 +1,43 @@
+import { PrismaClient } from "@prisma/client";
 import { ICatalogRepository } from "../interface/catalogRepository.interface";
 import { Product } from "../models/product.model";
-import { ProductFactory } from "../utils/fixtures";
 
 export class CatalogRepository implements ICatalogRepository {
+  _prisma: PrismaClient;
+
+  constructor() {
+    this._prisma = new PrismaClient();
+  }
+
   async create(data: Product): Promise<Product> {
-    const product = ProductFactory.build();
-    return Promise.resolve(product);
+    return this._prisma.product.create({
+      data,
+    });
   }
   async update(data: Product): Promise<Product> {
-    const product = ProductFactory.build();
-    return Promise.resolve(product);
+    return this._prisma.product.update({
+      where: { id: data.id },
+      data,
+    });
   }
   async delete(id: any) {
-    const product = ProductFactory.build();
-    return Promise.resolve(product);
+    return this._prisma.product.delete({
+      where: { id },
+    });
   }
   async find(limit: number, offset: number): Promise<Product[]> {
-    const products = ProductFactory.buildList(limit);
-    return Promise.resolve(products);
+    return this._prisma.product.findMany({
+      take: limit,
+      skip: offset,
+    });
   }
   async findOne(id: number): Promise<Product> {
-    const product = ProductFactory.build();
-    return Promise.resolve(product);
+    const product = await this._prisma.product.findFirst({
+      where: { id },
+    });
+    if (product) {
+      return Promise.resolve(product);
+    }
+    throw new Error("product not found");
   }
 }
